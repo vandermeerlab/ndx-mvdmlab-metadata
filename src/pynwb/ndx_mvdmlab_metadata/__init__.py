cddc277fba88bdfdc6378daf6a91ac3a73855270
Manish Mohapatra <manishofyore@gmail.com>
2025-02-28 06:17:11 -0500

    Update __init__.py

--- src/pynwb/ndx_mvdmlab_metadata/__init__.py
+++ src/pynwb/ndx_mvdmlab_metadata/__init__.py
@@ -7,7 +7,7 @@ spec_path = os.path.abspath(os.path.dirname(__file__))
 ns_path = os.path.join(spec_path, 'spec', f'{name}.namespace.yaml')
 
 load_namespaces(ns_path)
-
+LabMetaDataExtension = get_class('LabMetaDataExtension', name)
 ProbeExtension = get_class('ProbeExtension', name)
 OdorantInfoExtension = get_class('OdorantInfoExtension', name)
 ExperimentalBlockExtension = get_class('ExperimentalBlockExtension', name)
